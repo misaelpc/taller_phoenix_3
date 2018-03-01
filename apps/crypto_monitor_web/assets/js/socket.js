0b79b5c34ef85412c589e0ae8c2134c5c44f7a68
misaelpc <misaelpcgm@gmail.com>
2018-03-01 17:30:10 -0600

    Join Channel

--- apps/crypto_monitor_web/assets/js/socket.js
+++ apps/crypto_monitor_web/assets/js/socket.js
@@ -4,4 +4,19 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
+class LiveUpdate {
+  constructor() {
+    this.channel = socket.channel("ex_monitor:rates", {})
+  }
+
+  join(){
+    this.channel.join()
+      .receive("ok", resp => { console.log("Joined successfully", resp) })
+      .receive("error", resp => { console.log("Unable to join", resp) })
+  }
+}
+
+let liveUpdate = new LiveUpdate
+liveUpdate.join()
+
 export default socket
